4c4a7be63c2a993afca12b1865bb6d3242e6448d
Hisham Malik <hisham@arkhitech.com>
2018-03-01 18:37:20 +0500

    Update mercury_controller.rb
    Rails 5.1 compatibility
    
    fixed up rails 5 compatibility for mercury and others

--- app/controllers/mercury_controller.rb
+++ app/controllers/mercury_controller.rb
@@ -6,24 +6,24 @@ class MercuryController < ActionController::Base
   layout false
 
   def edit
-    render :text => '', layout: 'mercury'
+    render layout: 'mercury'
   end
 
   def resource
-    render :action => "/#{params[:type]}/#{params[:resource]}"
+    render action: "/#{params[:type]}/#{params[:resource]}"
   end
 
   def snippet_options
     @options = params[:options] || {}
-    render :action => "/snippets/#{params[:name]}/options"
+    render action: "/snippets/#{params[:name]}/options"
   end
 
   def snippet_preview
-    render :action => "/snippets/#{params[:name]}/preview"
+    render action: "/snippets/#{params[:name]}/preview"
   end
 
   def test_page
-    render :text => params
+    render plain: params
   end
 
   private
